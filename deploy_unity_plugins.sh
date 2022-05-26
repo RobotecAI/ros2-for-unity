ab5926a341d43e7b3fc25c38da403d18061d39d5
Piotr Jaroszek <piotr.jaroszek@robotec.ai>
2022-05-26 09:40:42 +0200

    Copy _py.so files for huble standalone

--- deploy_unity_plugins.sh
+++ deploy_unity_plugins.sh
@@ -16,5 +16,5 @@ pluginDir=$1
 mkdir -p  ${pluginDir}/Linux/x86_64/
 find install/lib/dotnet/ -maxdepth 1 -not -name "*.pdb" -type f -exec cp {} ${pluginDir} \;
 cp $SCRIPTPATH/install/standalone/* ${pluginDir}/Linux/x86_64/
-find install/lib/ -maxdepth 1 -not -name "*_py.so" -not -name "*_python.so" -type f -exec cp {} ${pluginDir}/Linux/x86_64/ \;
+find install/lib/ -maxdepth 1 -not -name "*_python.so" -type f -exec cp {} ${pluginDir}/Linux/x86_64/ \;
 cp $SCRIPTPATH/install/resources/*.so ${pluginDir}/Linux/x86_64/
